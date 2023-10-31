469d16f8bc98a860f830ddea7a16f5f7ac11ddc1
Andrei Luchici <andrei.luchici@daciandata.tech>
2023-10-31 11:12:02 +0200

    JS loops arrays and functions

--- cse/signup.js
+++ cse/signup.js
@@ -109,7 +109,168 @@ for (let i = start_value; i <= end_value; i++) {
 console.log(product);
 
 
+// while loop (when you want to loop until something is false)
+// while (condition is true) {
+//     statements
+// }
+// while (true) {
+//     console.log("While loop.")
+// }
+n = 20;
+sum = 0;
+let i = 0;
+while (i <= n) {
+    sum = sum + i;
+    i = i + 1;
+}
+console.log(sum);
+
+// do...while 
+// do {
+//     statements;
+// } while (condition is true);
+
+let result = "";
+i = 0;
+
+do {
+    i = i + 1;
+    result = result + i;
+} while (i < 5);
+console.log(result);
+
+// Array 
+const array1 = [];
+array1.push(1);
+
+const array2 = [];
+for (let i = 1; i <= 10; i++) {
+    array2.push(i);
+}
+console.log(array2);
+
+const indexOf2 = array2.indexOf(2);
+console.log("index of 2 = ", indexOf2);
+
+console.log("number of elements = ", array2.length);
+
+array2[0] = 10;
+console.log(array2);
+
+// check how many elements are even inside an array
+// 1. initialize an array 
+// 2. initialize a counter, countOfEven = 0
+// 3. 
+//  - go through this array element by element
+//  - check if current element is even (divide by 2 and reminder is 0)
+//  - if element is even increase counter by 1 
+const arr = [1, 2, 3, 6, 6, 10];
+let countOfEven = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        countOfEven = countOfEven + 1;
+    }
+}
+console.log("number of even elements = ", countOfEven);
+
+countOfEven = 0;
+for (let element of arr) {
+    if (element % 2 === 0) {
+        countOfEven += 1;
+    }
+}
+console.log("number of even elements = ", countOfEven);
+
+const obj1 = {
+    "name": "Web Apps 1",
+    "year": "2023-2024"
+};
+
+console.log(obj1["name"]);
+console.log(obj1.name);
+
+const data = {
+    "_id": "6540bdf66339efa474a049e3",
+    "index": 0,
+    "guid": "cb66e453-aafb-449a-b128-66b3c4b2e6bf",
+    "isActive": true,
+    "balance": "$3,278.15",
+    "picture": "http://placehold.it/32x32",
+    "age": 24,
+    "eyeColor": "green",
+    "name": "Maxine Green",
+    "gender": "female",
+    "company": "LUNCHPAD",
+    "email": "maxinegreen@lunchpad.com",
+    "phone": "+1 (954) 524-3798",
+    "address": "693 Manhattan Avenue, Bayview, Nebraska, 645",
+    "about": "Pariatur excepteur non aliquip mollit consequat quis dolor deserunt id irure enim ex occaecat. Est amet ipsum voluptate esse cillum officia consectetur ex incididunt. Sit commodo sunt cillum eu id magna laborum reprehenderit eiusmod tempor commodo.\r\n",
+    "registered": "2017-04-22T10:55:50 -03:00",
+    "latitude": -75.384207,
+    "longitude": -83.140368
+}
+
+console.log(data);
+console.log(JSON.stringify(data));
+
+const jsonString = JSON.stringify(data);
+
+const jsonObj = JSON.parse(jsonString);
+console.log(jsonObj.address);
+
+// function
+// function nameOfTheFunction(param1, param2, ..., paramN) {
+//     statements; // body of the function
+//     return value; // optional
+// }
+
+function isEven(value) {
+    // return value % 2 === 0;
+    if (value % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function countEvens(arr) {
+    let countOfEven = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            countOfEven = countOfEven + 1;
+        }
+    }
+    return countOfEven;
+}
+
+function countByCondition(arr, condition) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const countElement = condition(arr[i]);
+        if (countElement) {
+            count = count + 1;
+        }
+    }
+    return count;
+}
 
 
+let count = countEvens(array2);
+console.log("array 2 even numbers = ", count);
 
 
+count = countByCondition(array2, isEven);
+console.log("array 2 even numbers = ", count);
+
+function isGreaterThan5(value) {
+    return value > 5;
+}
+
+count = countByCondition(array2, isGreaterThan5);
+console.log("array 2 numbers > 5 = ", count);
+
+const elementsGreaterThan5 = array2.filter(isGreaterThan5);
+console.log(elementsGreaterThan5);
+
+const filteredValues = array2.filter(v => v > 5);
+console.log(filteredValues);
