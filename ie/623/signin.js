6f1cead619ac3b14daffc407489389aae05eec83
Andrei Luchici <andrei.luchici@daciandata.tech>
2023-11-21 09:42:54 +0200

    Interact JS

--- ie/623/signin.js
+++ ie/623/signin.js
@@ -1,134 +1,77 @@
-console.log("Welcome to signin!");
+// Obiectiv:
+// 1. Preluam informatiile din inputs 
+// 2. Verificam daca inputs sunt goale
+// 3. Schimbam border-color in rosu daca un input este invalid 
+// 4. Daca ambele inputuri sunt valide, afisam datele introduse pe ecran 
+// 5. Daca avem erori, afisam un mesaj de eroare sub ultimul paragraf. 
 
-let primaVariabila = 10;
-primaVariabila = 20;
-
-const aDouaVariabila = 10.3456;
-
-const aTreiaVariabila = "acesta este un sir de caractere";
-const aPatraVariabila = 'acesta este un sir de caractere';
-const stringInterpolat = `acesta este un sir de caractere ${primaVariabila}`;
-console.log(stringInterpolat);
-
-const obj = {
-    name: "IE",
-    "desc": "Curs",
-    'year': 2023,
-    "numeleCursului": "Web Apps"
-};
-
-console.log(obj.name);
-obj.desc = "Seminar";
-
-obj["year"] = "2023-2024";
-
-const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const arr2 = [obj, 10, 20, 40];
-
-console.log(obj);
-console.log(arr1);
-console.log(arr2);
-
-arr1.fill(10, 2, 5);
-console.log(arr1);
-
-arr1[1] = -10;
-console.log(arr1);
-
-console.log(arr1.indexOf(1));
-// &&, ||, !, ==, !=, ===, !==
-// if (conditie) {
-//     ...
-// } else if (alta conditie) {
-//     ...
-// } else {
-//     ...
-// }
-
-// for (let i = startValue; i <= endValue; i++) {
-//     ...
-// }
-// for (let value of array) {
-//    ...
-//} 
-for (let el of arr1) {
-    console.log(el);
+// get data from local and session storage
+let nClicks = localStorage.getItem("nClicks");
+// let nClicks = sessionStorage.getItem("nClicks");
+if (nClicks) {
+    nClicks = parseInt(nClicks);
+} else {
+    nClicks = 0;
 }
 
-// while (conditie) {
-//    ...
-// }
+// save to local and session storage 
+localStorage.setItem('nClicks', nClicks);
+sessionStorage.setItem('nClicks', nClicks);
 
-// do {
-//     ...
-// } while (conditie)
+function showErrorMessage() {
+    nClicks = nClicks + 1;
+    localStorage.setItem('nClicks', nClicks);
+    sessionStorage.setItem('nClicks', nClicks);
 
-// find all unique values in an array 
-// 1. initializam array-ul
-// 2. initializam un contor, array 
-// 3. parcurgem elementele array-ului, unul cate unul 
-// 3.1. verific daca elementul curent exista in contor
-// 3.1.1. pentru fiecare element din contor
-// 3.1.2. verific daca elementul curent == elementul curent din contor 
-// 3.1.3. daca sunt egale => exista 
-// 3.2. daca exista, merg mai departe la urmatorul element
-// 3.3. daca nu exista, il adaug in contor 
-const mainArray = [1, 2, 3, 3, 3, 4, 5, 7, 2, 1, 1];
-const contor = [];
-const counter = {};
-for (const el of mainArray) {
-    // find all unique elements
-    let ok = false; 
-    for (const cEl of contor) {
-        if (el === cEl) {
-            ok = true;
-            break;
-        }
-    }
+    // check if p element exists
+    const existingErrorParagraph = document.getElementById("errorParagraph");
+    if (existingErrorParagraph) {
+        existingErrorParagraph.innerText = `Ai completat gresit de ${nClicks} ori`; 
+        return;
+    } 
 
-    if (ok === false) {
-        contor.push(el);
-    }
+    // 1. Definim elemente noi, le customizam, le inseram in pagina 
+    // 1.1. definim elementul 
+    const p = document.createElement("p");
 
-    // count how many times each unique el appears in array
-    if (counter[el]) {
-        counter[el] = counter[el] + 1;
-    } else {
-        counter[el] = 1;
+    // 1.2. customizam elementul 
+    p.innerText = `Ai completat gresit de ${nClicks} ori`; // schimb mesajul
+    p.id = "errorParagraph";
+    // p.classList.add("animation");
+    // p.classList.add("pos-text");
+    p.style = "grid-column: 2; grid-row: 6;"
+
+    // 1.3. identificam parintele elementului 
+    const parent = document.getElementsByClassName("container")[0];
+
+    // 1.4. inseram in DOM 
+    parent.appendChild(p);
+
+    // 2. inseram inaintea unui element 
+    const neighbor = document.getElementsByClassName("pos-text")[0];
+    neighbor.style = "grid-row: 7";
+
+    parent.insertBefore(p, neighbor);
+}
+
+// 3. Modificam elemente existente (extragem elementul din DOM, il customizam)
+function changeTitle() {
+    // Modificam tag h1 
+    // - schimbam valoarea in "Sign in"
+    // - schimbam culoarea in albastru
+    // - schimbam textul in Arial 
+    const h1 = document.getElementById("title");
+    console.log(h1);
+    h1.innerText = "Sign in";
+    h1.style.color = "blue";
+    h1.style.fontFamily = "Arial";
+}
+
+function changeInputs() {
+    const inputs = document.getElementsByTagName("input");
+    console.log(inputs);
+
+    for (const input of inputs) {
+        input.classList.add("animation");
     }
 }
-
-console.log(mainArray);
-console.log(contor);
-console.log(counter);
-
-// function numeleFunctiei(param1, param2, param3) {
-//     ...
-//     return value;
-// }
-function greaterThan5(n) {
-    return n > 5;
-}
-
-function isEven(n) {
-    return n % 2 == 0;
-}
-
-function filter(arr, conditional) {
-    const result = [];
-    for (const el of arr) {
-        const keepElement = conditional(el);
-        if (keepElement) {
-            result.push(el);
-        }
-    }
-    return result;
-}
-
-const result = filter(arr1, isEven)
-console.log(result);
-
-const result2 = filter(result, greaterThan5);
-console.log(result2);
-
-const f = isEven;
